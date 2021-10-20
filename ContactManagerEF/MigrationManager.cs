10773c918c7edac5276e8a62260fbff3335f547a
progressor21 <adanilin@danilin.us>
2021-10-20 01:11:13 -0400

    Added MigrateDatabase extension method to migrate/create ContactManagerEF database and seed the demo data when the application starts

---
+++ ContactManagerEF/MigrationManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContactManagerEF.CMData;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ContactManagerEF
+{
+    public static class MigrationManager
+    {
+        public static IHost MigrateDatabase(this IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                using (var appContext = scope.ServiceProvider.GetRequiredService<ContactManagerContext>())
+                {
+                    try
+                    {
+                        appContext.Database.Migrate();
+                    }
+                    catch (Exception ex)
+                    {
+                        //Log errors or do anything you think it's needed
+                        throw;
+                    }
+                }
+            }
+
+            return host;
+        }
+    }
+}
--- ContactManagerEF/Program.cs
+++ ContactManagerEF/Program.cs
@@ -13,7 +13,7 @@ namespace ContactManagerEF
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().MigrateDatabase().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
